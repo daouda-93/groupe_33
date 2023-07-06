90b7bae5903f7f4b96c5c493fd0b7441c26e8478
Zolby <zolomercier@gmail.com>
2023-07-06 14:21:57 +0100

    dépot du dossier boostrap et la maquette du site et fichier index par éloge

---
+++ forms/contact.php
@@ -0,0 +1,37 @@
+<?php
+
+
+  // oouedraogo.ext@simplon.com est addresse l'email with your real receiving email  
+  
+  $receiving_email_address = 'oouedraogo.ext@simplon.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Impossible de charger la bibliothèque "PHP Email Form"!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Décommentez le code ci-dessous si vous souhaitez utiliser SMTP pour envoyer des e-mails. Vous devez entrer vos identifiants SMTP corrects
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
